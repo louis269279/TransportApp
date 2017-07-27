b7cd2dfb3180b82eca96f254d517009cc90745e7
root <root@ip-172-31-12-184.ap-southeast-2.compute.internal>
2017-07-27 11:53:26 +1000

    Updated script to write output to file

--- first.py
+++ first.py
@@ -4,14 +4,22 @@ import gtfs_realtime_pb2
 import urllib2
 import sys
 import csv
+import collections
+import time
 from datetime import datetime, timedelta
 
 BUS_STOP_MAP = {}
+leaveText = ""
+goText = ""
+
 def main():
-    if (len(sys.argv) != 4 or ((sys.argv[2] == 'sydneytrains' or sys.argv[2] == 'buses') == False)):
+    global leaveText, goText
+    if (len(sys.argv) != 3 or ((sys.argv[2] == 'sydneytrains' or sys.argv[2] == 'buses') == False)):
+        print("Error with arguments.")
         sys.exit()
 
-    print("[%s] Application Started." % datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    goText += "Result as at [%s]\n" % datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    leaveText += "Result as at [%s]\n" % datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
 
     header = None
     f = open('complete_gtfs/stops.txt', 'rb')
@@ -23,9 +31,9 @@ def main():
             BUS_STOP_MAP[row[0]] = row[2]
 
     if (sys.argv[1] == '-t'):
-        tripupdate(sys.argv[2], sys.argv[3])
+        tripupdate(sys.argv[2])
     elif (sys.argv[1] == '-v'):
-        vehiclepos(sys.argv[2], sys.argv[3])
+        vehiclepos(sys.argv[2])
 
 def vehiclepos(transportType):
     feed = gtfs_realtime_pb2.FeedMessage()
@@ -48,12 +56,16 @@ def vehiclepos(transportType):
             if (entity.id.split("_")[3] == '370'):
                 print(entity)
 
-def tripupdate(transportType, travelDirection):
+def tripupdate(transportType):
     feed = gtfs_realtime_pb2.FeedMessage()
     opener = urllib2.build_opener()
     opener.addheaders = [('Authorization', 'apikey 0s1qgK2KAe2F2PUlZga9Ew1JXbuQ52dbWvQn')]
+
+    start_time = time.time()
     response = opener.open('https://api.transport.nsw.gov.au/v1/gtfs/realtime/' + transportType)
     feed.ParseFromString(response.read())
+    print("Time taken to send request and parse response: %s seconds" % (time.time() - start_time))
+
 
     if (transportType == 'sydneytrains'):
         for entity in feed.entity:
@@ -61,26 +73,49 @@ def tripupdate(transportType, travelDirection):
                 print(entity)
 
     elif (transportType == 'buses'):
+        global leaveText, goText
         results = []
+        bus_trip_dets = collections.defaultdict(str)
         for entity in feed.entity:
+            active = False
             if (entity.id.split("_")[3] == '370'):
                 for stop_time_update in entity.trip_update.stop_time_update:
-                    if (stop_time_update.stop_id == '203114'):
+                    bus_trip_dets[entity.id] += "\n[Arrival: " + datetime.fromtimestamp(int(stop_time_update.arrival.time)).strftime('%Y-%m-%d %H:%M:%S') + "] " + (BUS_STOP_MAP[stop_time_update.stop_id])
+                    if ((stop_time_update.stop_id == '203114')
+                            or (stop_time_update.stop_id == '201715')):
 
                         now = datetime.now()
                         then = datetime.fromtimestamp(int(stop_time_update.arrival.time))
                         tdelta = then - now
-                        hours, remainder = divmod(tdelta.seconds, 3600)
                         #minutes, seconds = divmod(remainder, 60)
-                        #seconds = tdelta.total_seconds()
                         #print ("Time till next bus: %s minutes %s seconds" % divmod(remainder, 60))
-                        results.append(remainder)
+                        results.append((entity.id, tdelta.seconds, stop_time_update.stop_id))
+                        active = True
                         #print(datetime.fromtimestamp(int(stop_time_update.arrival.time)).strftime('%Y-%m-%d %H:%M:%S'))
                         #stop_time_update.stop_id += ' ' + BUS_STOP_MAP[stop_time_update.stop_id]
+                    
+                    elif ((active and stop_time_update.stop_id == '201511')
+                            or (active and stop_time_update.stop_id == '203255')):
+                        break
                     #print(entity)
 
-        results.sort()
-        for remainder in results:
-            print ("Time till next bus: %s minutes %s seconds" % divmod(remainder, 60))
+        results.sort(key=lambda tup: tup[1])
+        for (entity_id, total_seconds, stop_id) in results:
+            hours, remainder = divmod(total_seconds, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            if stop_id == '203114':
+                leaveText += ("Time till next bus: %s hours %s minutes %s seconds" % (hours, minutes, seconds))
+                leaveText += (bus_trip_dets[entity_id])
+                leaveText += ("\n--------------------\n\n")
+            elif stop_id == '201715':
+                goText += ("Time till next bus: %s hours %s minutes %s seconds" % (hours, minutes, seconds))
+                goText += (bus_trip_dets[entity_id])
+                goText += ("\n--------------------\n\n")
+
+    with open("370LeaveUni.txt", "w") as f:
+        f.write(leaveText)
+
+    with open("370GoToUni.txt", "w") as f:
+        f.write(goText) 
 
 main()
